a6616f37e8a0f9d4b9e57443ea2fdc3188ed537f
Emma Wiggans <ea.wiggans@gmail.com>
2023-01-30 09:18:57 -0500

    started mutations on client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,20 +6,47 @@ export const QUERY_USER = gql`
       _id
       username
       email
-      savedBooks
-    }
-  }
-`;
-
-export const QUERY_SAVEDBOOKS = gql`
-  query savedBooks($bookId: String) {
-    savedBooks(bookId: $bookId) {
+    savedBooks {
+      bookId
       authors
       description
-      bookId
       image
       link
       title
     }
+      bookCount
+    }
   }
 `;
+
+export const QUERY_ME = gql`
+  query me {
+    user {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+      bookCount
+    }
+  }
+`;
+
+// export const QUERY_SAVEDBOOKS = gql`
+//   query savedBooks($bookId: String) {
+//     savedBooks(bookId: $bookId) {
+//       authors
+//       description
+//       bookId
+//       image
+//       link
+//       title
+//     }
+//   }
+// `;
